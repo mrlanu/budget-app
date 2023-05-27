69a43a2b7b47eea085d7817a0a479d9b715e997a
mrlanu <mrlanu@gmail.com>
2023-05-26 20:53:34 -0500

    fix: reload edited transaction

--- lib/transactions/cubit/transactions_cubit.dart
+++ lib/transactions/cubit/transactions_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -9,27 +11,46 @@ part 'transactions_state.dart';
 class TransactionsCubit extends Cubit<TransactionsState> {
   final TransactionsRepository _transactionsRepository;
   final String _budgetId;
+  late final StreamSubscription<List<Transaction>> _transactionsSubscription;
 
   TransactionsCubit({
     required String budgetId,
     required TransactionsRepository transactionsRepository,
   })  : _transactionsRepository = transactionsRepository,
         _budgetId = budgetId,
-        super(TransactionsState());
+        super(TransactionsState()) {
+    _transactionsSubscription = _transactionsRepository
+        .getTransactions()
+        .skip(1)
+        .listen((transactions) {
+      fetchTransactions(
+          categoryId: state.lastCategoryId,
+          date: state.lastDate ?? DateTime.now());
+    });
+  }
 
   Future<void> fetchTransactions(
       {required categoryId, required DateTime date}) async {
     emit(state.copyWith(status: TransactionsStatus.loading));
     try {
       final result = await _transactionsRepository.fetchAllTransactions(
-          budgetId: _budgetId,
-          dateTime: date,
-          categoryId: categoryId);
-      emit(state.copyWith(
-          status: TransactionsStatus.success, transactionList: result));
+          budgetId: _budgetId, dateTime: date, categoryId: categoryId);
+      emit(
+        state.copyWith(
+            status: TransactionsStatus.success,
+            transactionList: result,
+            lastCategoryId: categoryId,
+            lastDate: date),
+      );
     } catch (e) {
       emit(state.copyWith(
           status: TransactionsStatus.failure, errorMessage: e.toString()));
     }
+  }
+
+  @override
+  Future<void> close() {
+    _transactionsSubscription.cancel();
+    return super.close();
   }
 }
--- lib/transactions/cubit/transactions_state.dart
+++ lib/transactions/cubit/transactions_state.dart
@@ -6,27 +6,35 @@ class TransactionsState extends Equatable {
 
   final TransactionsStatus status;
   final List<Transaction> transactionList;
+  final String lastCategoryId;
+  final DateTime? lastDate;
   final String? errorMessage;
 
   const TransactionsState({
     this.status = TransactionsStatus.initial,
     this.transactionList = const [],
+    this.lastDate,
+    this.lastCategoryId = '',
     this.errorMessage,
 });
 
   TransactionsState copyWith({
     TransactionsStatus? status,
     List<Transaction>? transactionList,
+    String? lastCategoryId,
+    DateTime? lastDate,
     String? errorMessage,
   }) {
     return TransactionsState(
       status: status ?? this.status,
       transactionList: transactionList ?? this.transactionList,
+      lastCategoryId: lastCategoryId ?? this.lastCategoryId,
+      lastDate: lastDate ?? this.lastDate,
       errorMessage: errorMessage ?? this.errorMessage,
     );
   }
 
   @override
-  List<Object?> get props => [status, transactionList, errorMessage];
+  List<Object?> get props => [status, transactionList, lastCategoryId, lastDate, errorMessage];
 }
 
