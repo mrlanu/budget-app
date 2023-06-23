a5e83a1f21d142e8fc4c2ea86b3e905f5548be5c
mrlanu <mrlanu@gmail.com>
2023-06-23 17:46:23 -0500

    feat: add firebase for web

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:bloc/bloc.dart';
 import 'package:budget_app/app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = const AppBlocObserver();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(App());
 }
