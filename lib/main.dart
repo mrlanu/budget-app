b8b8845c49f5fe840ffd9517815b3d42b6e49dfa
mrlanu <mrlanu@gmail.com>
2024-04-06 06:38:40 -0500

    build: upgrade firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:budget_app/app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
-import 'constants/colors.dart';
+import 'app/bloc_observer.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -16,15 +15,7 @@ Future<void> main() async {
         ? HydratedStorage.webStorageDirectory
         : await getTemporaryDirectory(),
   );
-  //Bloc.observer = const AppBlocObserver();
-  /*SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    systemNavigationBarColor: BudgetColors.systemBar,
-    systemNavigationBarIconBrightness: Brightness.light,
-    systemNavigationBarDividerColor: null,
-    statusBarColor: BudgetColors.statusBar,
-    statusBarIconBrightness: Brightness.light,
-    statusBarBrightness: Brightness.light,
-  ));*/
+  Bloc.observer = const AppBlocObserver();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
