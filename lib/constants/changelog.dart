ee73b904afa94cd4d4bc102dfed858bd95b81f92
mrlanu <mrlanu@gmail.com>
2025-05-23 10:59:19 -0500

    fix: first day of chart's range

--- lib/constants/changelog.dart
+++ lib/constants/changelog.dart
@@ -1,5 +1,14 @@
 const changelog = [
   {
+    'version': 'v4.?.?',
+    'date': '??? ??, ????',
+    'titles': [],
+    'added': [],
+    'fixed': [
+      'First month of the chart\'s range started with wrong day'
+    ],
+  },
+  {
     'version': 'v4.6.0',
     'date': 'May 23, 2025',
     'titles': [],
